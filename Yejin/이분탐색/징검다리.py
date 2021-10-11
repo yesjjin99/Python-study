d8f4f2f146ba74bc0222347b22b134d99a555946
yesjjin99 <yesjjin99@naver.com>
2021-10-11 20:31:11 +0900

    2주차 완전탐색 & 이분탐색

---
+++ Yejin/이분탐색/징검다리.py
@@ -0,0 +1,27 @@
+def solution(distance, rocks, n):
+    answer = 0
+    start = 0
+    end = distance
+    rocks.sort()
+    
+    while start <= end:
+        mid = (start + end) // 2
+        # mid가 거리의 최솟값이라고 가정
+        remove = 0
+        prev = 0
+        for rock in rocks:
+            if rock - prev < mid:
+                remove += 1
+            else:
+                prev = rock
+            
+            if remove > n:
+                break
+        if remove > n:
+            end = mid - 1
+        else:
+            answer = mid
+            # 최솟값 중 가장 큰 값
+            start = mid + 1
+            
+    return answer
