a65d224b7304580f37429eb81686e959e271228e
yesjjin99 <yesjjin99@naver.com>
2021-10-25 17:38:29 +0900

    3주차 Greedy & DP

---
+++ Yejin/DP/정수삼각형.py
@@ -0,0 +1,12 @@
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        n = len(triangle[i])
+        for j in range(n):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == n - 1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+    answer = max(triangle[-1])
+    return answer
